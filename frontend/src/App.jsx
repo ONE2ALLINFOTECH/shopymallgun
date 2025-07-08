3c9a4b93b85bba64763b01d7b1504aafb6475a88
ONE2ALLINFOTECH <one2allinfotech@gmail.com>
2025-07-08 11:44:02 +0530

    Update: your message here

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -36,7 +36,7 @@ const App = () => {
   return (
     <Router>
       <div className="d-flex">
-        <div >
+        <div className="flex-grow-1" style={{ marginLeft: "250px" }}>
           <Routes>
             {/* Public Routes */}
             <Route path="/" element={<Register />} />
