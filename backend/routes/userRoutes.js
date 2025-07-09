5180ca580ac1f4a7cb62b299350652713ea8ff33
ONE2ALLINFOTECH <one2allinfotech@gmail.com>
2025-07-09 12:56:06 +0530

    Initial commit

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,8 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { sendOTP, verifyOTP, registerUser } = require("../controllers/userController");
 const { saveProfileInfo } = require("../controllers/userController");
-const { aadhaarKYC, verifyAadhaarOTP } = require("../controllers/userController");
-const { verifyPAN } = require("../controllers/userController");
+
 const {
   sendEmailOTP,
   verifyEmailOTP,
@@ -16,9 +15,7 @@ router.post("/send-otp", sendOTP);
 router.post("/verify-otp", verifyOTP);
 router.post("/register", registerUser);
 router.post("/profile-info", saveProfileInfo);
-router.post("/aadhaar/send-otp", aadhaarKYC);
-router.post("/aadhaar/verify-otp", verifyAadhaarOTP);
-router.post("/pan/verify", verifyPAN);
+
 router.post("/forgot/send-otp", sendEmailOTP);
 router.post("/forgot/verify-otp", verifyEmailOTP);
 router.post("/forgot/reset-password", resetPassword);
